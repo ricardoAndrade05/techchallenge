cba41f2763de65ac7132d584f3cc8b93bcb518a8
ricardo.andrade <ricardo.andrade05@gmail.com>
2026-07-16 13:13:59 -0300

    Commit inicial

---
+++ src/main/java/com/desafio/postech/delivery/PostechApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.postech.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/postech/delivery/PostechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.postech.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
